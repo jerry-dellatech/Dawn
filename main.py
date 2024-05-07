1bdc6072058283a66dd61c75777ed4fdcae35178
jerry-dellatech <dr.jerry.l.martin@gmail.com>
2024-05-06 19:49:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,26 @@ import pins
 from valve import Valve
 from pump import VacuumPump
 from time import sleep
+from machine import WDT # watchdog timer
 
+wdt = WDT(timeout=4000) # start watchdog with timeout of 4s
 """Main file. Handles physical pin inputs; toggles valves & vacuum pump"""
 
 def main():
+    print ("starting")
     # main loop
     while True:
+        wdt.feed()
         if pins.START.pullup_value() and pins.STOP.pullup_value(): 
             continue # IO conflict, ignore it
         
         if pins.START.pullup_value():
+            print("got start command")
             pump.set_pump(VacuumPump.ON)
             sleep(0.1)
             valve.set_valve(Valve.OPEN)
         elif pins.STOP.pullup_value():
+            print("got stop command")
             valve.set_valve(Valve.CLOSED)
             sleep(0.1)
             pump.set_pump(VacuumPump.OFF)
